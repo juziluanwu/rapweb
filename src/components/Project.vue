fcbe32bf680c28689e2b440ad870b4a8de2c3e10
juziluanwu <782265504@qq.com>
2019-07-24 22:16:10 +0800

    rap

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -2,32 +2,59 @@
   <div id="v-project" style="width: 600px;margin: auto">
     <el-container>
       <el-header>
-        <h1>  接口文档</h1>
+        <h1>接口文档</h1>
       </el-header>
       <el-main>
         <el-row>
-          <el-col :span="12"><div class="grid-content bg-purple"><el-input v-model="projectinfo" placeholder="请输入内容"></el-input></div></el-col>
-          <el-col :span="12"><div class="grid-content bg-purple-light"> <el-button @click="searchProject()">搜索</el-button></div></el-col>
+          <el-col :span="16"><el-input v-model="searchinfo" placeholder="请输入内容"></el-input></el-col>
+          <el-col :span="4"><el-button @click="searchProject()">搜索</el-button></el-col>
+          <el-col :span="4"><el-button @click="addProject()">新增</el-button></el-col>
         </el-row>
 
           <el-table
-                  :data="tableData"
+                  :data="projectinfo"
                   style="width: 100%">
             <el-table-column
-                    prop="date"
+                    fixed
+                    prop="name"
                     label="项目名称"
                     width="180">
             </el-table-column>
             <el-table-column
-                    prop="name"
+                    fixed="right"
                     label="操作"
                     width="180">
+              <template slot-scope="scope">
+                <el-button @click="info_click(scope.row)" type="text" size="small">查看</el-button>
+                <el-button type="text" size="small">编辑</el-button>
+              </template>
             </el-table-column>
 
           </el-table>
 
       </el-main>
     </el-container>
+
+    <el-dialog title="编辑" :visible.sync="dialogFormVisible">
+      <el-form :model="projectdetail">
+        <el-form-item label="项目名称">
+          <el-input v-model="projectdetail.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="项目简介">
+          <el-input
+                  type="textarea"
+                  :rows="2"
+                  placeholder="请输入内容"
+                  v-model="projectdetail.remark">
+          </el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -39,20 +66,35 @@ export default {
   props: {
     msg: String
   },
-  data () {
+  data() {
     return {
-      info: null,
-      projectinfo:''
+      projectinfo: [],
+      searchinfo:'',
+      projectdetail:{
+        name:'',
+        remark:''
+      },
+      dialogFormVisible:false,
+      formLabelWidth: 20
     }
+  },
+  mounted(){
+    this.searchProject();
   },
   methods: {
     searchProject(){
-      this.$Axios.get('http://localhost:8080/rap/project/list')
-      .then(response => {
-        this.info = response.data;
-        console.log(this.info)
-
+      this.$Axios.get('http://localhost:8081/rap/project/list')
+      .then(res => {
+        this.projectinfo = res.data.data;
       })
+    },
+    info_click(row){
+      this.dialogFormVisible = true;
+      this.projectdetail.name=row.name;
+      this.projectdetail.remark=row.remark;
+    },
+    addProject(){
+
     }
   }
 }
