4d07b50ab4ff395980810609d83746d9f707151b
juziluanwu <782265504@qq.com>
2019-07-25 17:24:46 +0800

    rap

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,121 +1,179 @@
 <template>
-  <div id="v-project" style="width: 600px;margin: auto">
-    <el-container>
-      <el-header>
-        <h1>接口文档</h1>
-      </el-header>
-      <el-main>
-        <el-row>
-          <el-col :span="16"><el-input v-model="searchinfo" placeholder="请输入内容"></el-input></el-col>
-          <el-col :span="4"><el-button @click="searchProject()">搜索</el-button></el-col>
-          <el-col :span="4"><el-button @click="addProject()">新增</el-button></el-col>
-        </el-row>
+    <div id="v-project" style="width: 600px;margin: auto">
+        <el-container>
+            <el-header>
+                <h1>接口文档</h1>
+            </el-header>
+            <el-main>
+                <el-row>
+                    <el-col :span="16">
+                        <el-input v-model="searchinfo" placeholder="请输入内容"></el-input>
+                    </el-col>
+                    <el-col :span="4">
+                        <el-button @click="searchProject()">搜索</el-button>
+                    </el-col>
+                    <el-col :span="4">
+                        <el-button @click="showAddDig()">新增</el-button>
+                    </el-col>
+                </el-row>
 
-          <el-table
-                  :data="projectinfo"
-                  style="width: 100%">
-            <el-table-column
-                    fixed
-                    prop="name"
-                    label="项目名称"
-                    width="180">
-            </el-table-column>
-            <el-table-column
-                    fixed="right"
-                    label="操作"
-                    width="180">
-              <template slot-scope="scope">
-                <el-button @click="info_click(scope.row)" type="text" size="small">查看</el-button>
-                <el-button type="text" size="small">编辑</el-button>
-              </template>
-            </el-table-column>
+                <el-table
+                        :data="projectinfo"
+                        style="width: 100%">
+                    <el-table-column
+                            fixed
 
-          </el-table>
+                            label="项目名称"
+                            width="180">
+                        <template slot-scope="scope">
+                            <router-link to="/MenuDetail">{{scope.row.name}}</router-link>
+                        </template>
+                    </el-table-column>
+                    <el-table-column
+                            fixed="right"
+                            label="操作"
+                            width="180">
+                        <template slot-scope="scope">
+                            <el-button @click="showDetailDig(scope.row)" type="text" size="small">查看</el-button>
+                            <el-button @click="showEditDig(scope.row)" type="text" size="small">编辑</el-button>
+                        </template>
+                    </el-table-column>
 
-      </el-main>
-    </el-container>
+                </el-table>
 
-    <el-dialog title="编辑" :visible.sync="dialogFormVisible">
-      <el-form :model="projectdetail">
-        <el-form-item label="项目名称">
-          <el-input v-model="projectdetail.name" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="项目简介">
-          <el-input
-                  type="textarea"
-                  :rows="2"
-                  placeholder="请输入内容"
-                  v-model="projectdetail.remark">
-          </el-input>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-      </div>
-    </el-dialog>
+            </el-main>
+        </el-container>
 
-  </div>
+        <el-dialog title="操作" :visible.sync="dialogFormVisible">
+            <el-form :model="projectdetail">
+                <el-input v-model="projectdetail.id" type="hidden"></el-input>
+                <el-form-item label="项目名称:">
+                    <el-input v-model="projectdetail.name" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="项目简介:">
+                    <el-input
+                            type="textarea"
+                            :rows="2"
+                            placeholder="请输入内容"
+                            v-model="projectdetail.remark">
+                    </el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="addOrEdit()">确 定</el-button>
+            </div>
+        </el-dialog>
+
+
+        <el-dialog title="详情" :visible.sync="detailVisible">
+            <el-form :model="projectdetail">
+                <el-form-item label="项目名称:">
+                    <el-span>{{projectdetail.name}}</el-span>
+                </el-form-item>
+                <el-form-item label="项目简介:">
+                    <el-span>{{projectdetail.remark}}</el-span>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="detailVisible = false">取 消</el-button>
+                <el-button type="primary" @click="detailVisible = false">确 定</el-button>
+            </div>
+        </el-dialog>
+    </div>
 </template>
 
 <script>
 
-export default {
+    export default {
 
-  name: 'project',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      projectinfo: [],
-      searchinfo:'',
-      projectdetail:{
-        name:'',
-        remark:''
-      },
-      dialogFormVisible:false,
-      formLabelWidth: 20
+        name: 'project',
+        props: {
+            msg: String
+        },
+        data() {
+            return {
+                projectinfo: [],
+                searchinfo: '',
+                projectdetail: {
+                    id: '',
+                    name: '',
+                    remark: ''
+                },
+                dialogFormVisible: false,
+                detailVisible: false,
+                formLabelWidth: 20
+            }
+        },
+        mounted() {
+            this.searchProject();
+        },
+        methods: {
+            searchProject() {
+                this.$Axios.get(this.BASEURL + '/project/list?info='+this.searchinfo)
+                    .then(res => {
+                        this.projectinfo = res.data.data;
+                    })
+            },
+            showDetailDig(row) {
+                this.detailVisible = true;
+                this.projectdetail = row;
+            },
+            showEditDig(row) {
+                this.dialogFormVisible = true;
+                this.projectdetail = row;
+            },
+            showAddDig() {
+                this.dialogFormVisible = true;
+                //this.projectdetail=
+                this.projectdetail.id = '';
+                this.projectdetail.name = '';
+                this.projectdetail.remark = '';
+            },
+            addOrEdit() {
+                if (this.projectdetail.id != null && this.projectdetail.id != '') {
+                    this.$Axios.post(this.BASEURL + '/project/update', this.projectdetail)
+                        .then(res => {
+                                if (res.data.success) {
+                                    this.dialogFormVisible = false;
+                                    this.searchProject()
+                                }
+                            }
+                        )
+                } else {
+                    this.$Axios.post(this.BASEURL + '/project/save', this.projectdetail)
+                        .then(res => {
+                                if (res.data.success) {
+                                    this.dialogFormVisible = false;
+                                    this.searchProject()
+                                }
+                            }
+                        )
+                }
+            }
+        }
     }
-  },
-  mounted(){
-    this.searchProject();
-  },
-  methods: {
-    searchProject(){
-      this.$Axios.get('http://localhost:8081/rap/project/list')
-      .then(res => {
-        this.projectinfo = res.data.data;
-      })
-    },
-    info_click(row){
-      this.dialogFormVisible = true;
-      this.projectdetail.name=row.name;
-      this.projectdetail.remark=row.remark;
-    },
-    addProject(){
-
-    }
-  }
-}
 
 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -9,7 +9,13 @@ Vue.config.productionTip = false
 // Vue.use(Axios);
 
 Vue.prototype.$Axios = Axios;
+Vue.prototype.BASEURL='http://localhost:8080/rap';
+
+const router = new VueRouter({
+  routes // （缩写）相当于 routes: routes
+})
 
 new Vue({
+  // router,
   render: h => h(App),
 }).$mount('#app')
