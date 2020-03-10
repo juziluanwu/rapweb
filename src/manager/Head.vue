7e95737704e457587c4138121e5647583f2edffb
juziluanwu <782265504@qq.com>
2020-03-10 15:45:26 +0800

    rap

---
+++ src/manager/Head.vue
@@ -0,0 +1,37 @@
+<template>
+    <el-container>
+        <el-row>
+            <el-col :span="16">
+                <el-input v-model="version" placeholder="请填写接口版本"></el-input>
+            </el-col>
+            <el-col :span="4">
+                <el-button @click="confirmversion()">确认</el-button>
+            </el-col>
+        </el-row>
+    </el-container>
+</template>
+<script>
+    export default {
+        name: "VersionHead",
+        data() {
+            return {
+                version:''
+            }
+        },
+        mounted() {
+
+        },
+        methods: {
+            confirmversion(){
+                localStorage.setItem("version",this.version)
+                var version = localStorage.getItem("version");
+                if (version != null && version != '') {
+                    this.$message({
+                        message: '填写成功---当前版本'+version,
+                        type: 'success'
+                    });
+                }
+            }
+        }
+    }
+</script>
